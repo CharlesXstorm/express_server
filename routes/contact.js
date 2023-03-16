9728e59eb6f53c308a2ded7d04da251839eeae21
Charles Ukadike <charlesukadike44@gmail.com>
2023-03-16 15:34:56 +0100

    workng with css on expressjs

--- routes/contact.js
+++ routes/contact.js
@@ -1,13 +1,15 @@
 
 const express = require("express")
 const path = require("path")
+const file = require("fs")
 const rootDir = require("../utility/path")
 
 const router = express.Router()
 
 router.post("/admin/message",(req,res,next)=>{
-    console.log(req.body)
-    res.status(302).redirect("/")
+    //  console.log(typeof(req.body.message))
+    file.writeFileSync("message.txt",`name: ${req.body.client}\nemail: ${req.body.email}\nmessage: ${req.body.message}`);
+    res.redirect("/")
 })
 
 router.get("/admin/contact",(req,res,next)=>{
